a8d2e4ee1e7c7299ab6dd46a3d8ec4e1a49e60d5
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-16 03:21:59 +0300

    Update database.js

--- dok75-clinic-system/config/database.js
+++ dok75-clinic-system/config/database.js
@@ -1,1 +1,24 @@
-// database.js
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ✅ إعدادات قاعدة البيانات
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    logging: false
+});
+
+module.exports = sequelize;
