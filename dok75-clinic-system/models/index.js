af457162ae11fcf9362e31a726859e0f11c471b0
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-18 01:21:29 +0300

    Update index.js

--- dok75-clinic-system/models/index.js
+++ dok75-clinic-system/models/index.js
@@ -2,11 +2,10 @@ const { sequelize } = require('../config/database');
 
 // استيراد النماذج
 const Clinic = require('./Clinic')(sequelize);
-const Department = require('./Department')(sequelize);
 const User = require('./User')(sequelize);
 const Patient = require('./Patient')(sequelize);
 const Appointment = require('./Appointment')(sequelize);
-const Questionnaire = require('./Questionnaire')(sequelize);
+const Visit = require('./Visit')(sequelize);
 
 // ============================================
 // العلاقات
@@ -16,13 +15,21 @@ const Questionnaire = require('./Questionnaire')(sequelize);
 Clinic.hasMany(User, { foreignKey: 'clinic_id' });
 User.belongsTo(Clinic, { foreignKey: 'clinic_id' });
 
-// User (receptionist/doctor) -> Patient (creator)
+// Clinic -> Patient
+Clinic.hasMany(Patient, { foreignKey: 'clinic_id' });
+Patient.belongsTo(Clinic, { foreignKey: 'clinic_id' });
+
+// Clinic -> Appointment
+Clinic.hasMany(Appointment, { foreignKey: 'clinic_id' });
+Appointment.belongsTo(Clinic, { foreignKey: 'clinic_id' });
+
+// Clinic -> Visit
+Clinic.hasMany(Visit, { foreignKey: 'clinic_id' });
+Visit.belongsTo(Clinic, { foreignKey: 'clinic_id' });
+
+// User (creator) -> Patient
 User.hasMany(Patient, { foreignKey: 'created_by' });
 Patient.belongsTo(User, { foreignKey: 'created_by' });
-
-// Patient -> Appointment
-Patient.hasMany(Appointment, { foreignKey: 'patient_id' });
-Appointment.belongsTo(Patient, { foreignKey: 'patient_id' });
 
 // User (doctor) -> Appointment
 User.hasMany(Appointment, { foreignKey: 'doctor_id' });
@@ -32,20 +39,31 @@ Appointment.belongsTo(User, { foreignKey: 'doctor_id' });
 User.hasMany(Appointment, { foreignKey: 'receptionist_id' });
 Appointment.belongsTo(User, { foreignKey: 'receptionist_id' });
 
-// Patient -> Questionnaire
-Patient.hasMany(Questionnaire, { foreignKey: 'patient_id' });
-Questionnaire.belongsTo(Patient, { foreignKey: 'patient_id' });
+// User (creator) -> Appointment
+User.hasMany(Appointment, { foreignKey: 'created_by' });
+Appointment.belongsTo(User, { foreignKey: 'created_by' });
 
-// User (doctor) -> Questionnaire
-User.hasMany(Questionnaire, { foreignKey: 'doctor_id' });
-Questionnaire.belongsTo(User, { foreignKey: 'doctor_id' });
+// Patient -> Appointment
+Patient.hasMany(Appointment, { foreignKey: 'patient_id' });
+Appointment.belongsTo(Patient, { foreignKey: 'patient_id' });
+
+// Patient -> Visit
+Patient.hasMany(Visit, { foreignKey: 'patient_id' });
+Visit.belongsTo(Patient, { foreignKey: 'patient_id' });
+
+// User (doctor) -> Visit
+User.hasMany(Visit, { foreignKey: 'doctor_id' });
+Visit.belongsTo(User, { foreignKey: 'doctor_id' });
+
+// Appointment -> Visit
+Appointment.hasOne(Visit, { foreignKey: 'appointment_id' });
+Visit.belongsTo(Appointment, { foreignKey: 'appointment_id' });
 
 module.exports = {
     sequelize,
     Clinic,
-    Department,
     User,
     Patient,
     Appointment,
-    Questionnaire
+    Visit
 };
