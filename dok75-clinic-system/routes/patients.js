47378c99ef8079c71b141ffb419c640aa64364ca
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-18 01:24:29 +0300

    Update patients.js

--- dok75-clinic-system/routes/patients.js
+++ dok75-clinic-system/routes/patients.js
@@ -2,24 +2,38 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
 const { protect } = require('../middleware/auth');
-const { doctorOnly, receptionistOnly } = require('../middleware/roles');
+const { adminOnly, doctorOnly, receptionistOnly, doctorOwnPatient } = require('../middleware/roles');
 
 // جميع المسارات تتطلب توثيق
 router.use(protect);
 
 // إضافة مريض جديد (للاستقبال والدكتور)
-router.post('/', patientController.createPatient);
+router.post('/', receptionistOnly, patientController.createPatient);
 
-// عرض مرضى الدكتور فقط (للدكتور)
-router.get('/my-patients', doctorOnly, patientController.getDoctorPatients);
+// عرض جميع المرضى (للأدمن والاستقبال)
+router.get('/', (req, res, next) => {
+    if (req.user.role === 'doctor') {
+        return patientController.getDoctorPatients(req, res);
+    }
+    return patientController.getAllPatients(req, res);
+});
 
-// عرض جميع المرضى (للاستقبال)
-router.get('/all', receptionistOnly, patientController.getAllPatients);
+// عرض مريض محدد مع صلاحيات
+router.get('/:id', doctorOwnPatient, patientController.getPatientById);
 
-// عرض ملف مريض كامل
-router.get('/:id/record', patientController.getPatientFullRecord);
+// تحديث بيانات مريض (للاستقبال والأدمن)
+router.put('/:id', receptionistOnly, patientController.updatePatient);
+
+// حذف مريض (للأدمن فقط)
+router.delete('/:id', adminOnly, patientController.deletePatient);
 
 // البحث عن مريض
 router.get('/search/:query', patientController.searchPatients);
+
+// سجل زيارات المريض
+router.get('/:id/visits', doctorOwnPatient, patientController.getPatientVisits);
+
+// إحصائيات المرضى
+router.get('/stats/summary', patientController.getPatientStats);
 
 module.exports = router;
