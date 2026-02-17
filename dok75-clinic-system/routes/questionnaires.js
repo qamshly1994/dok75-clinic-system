4845e66516dfa6f7178f91d8f22a160c660d908a
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-17 05:39:17 +0300

    Update questionnaires.js

--- dok75-clinic-system/routes/questionnaires.js
+++ dok75-clinic-system/routes/questionnaires.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express.Router();
 const questionnaireController = require('../controllers/questionnaireController');
 const { protect } = require('../middleware/auth');
-const { doctorOnly } = require('../middleware/roles');
 
 // جميع المسارات تتطلب توثيق
 router.use(protect);
 
 // إنشاء استبيان جديد
-router.post('/', doctorOnly, questionnaireController.createQuestionnaire);
+router.post('/', questionnaireController.createQuestionnaire);
 
-// عرض استبيانات مريض معين
+// عرض استبيانات مريض
 router.get('/patient/:patientId', questionnaireController.getPatientQuestionnaires);
-
-// عرض استبيان محدد
-router.get('/:id', questionnaireController.getQuestionnaireById);
-
-// تحديث استبيان
-router.put('/:id', doctorOnly, questionnaireController.updateQuestionnaire);
 
 module.exports = router;
