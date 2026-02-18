36ff09d459e79de6de9eb65b75a796ac8d3cae5c
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-18 03:08:43 +0300

    Update patients.js

--- dok75-clinic-system/routes/patients.js
+++ dok75-clinic-system/routes/patients.js
@@ -10,22 +10,22 @@ router.use(protect);
 // إضافة مريض جديد (الكل)
 router.post('/', patientController.createPatient);
 
-// عرض جميع المرضى (للاستقبال والمشرف)
+// عرض جميع المرضى (للاستقبال والمشرف) - السطر 10
 router.get('/all', receptionistOnly, patientController.getAllPatients);
 
-// عرض مرضى الدكتور فقط (للدكتور)
+// عرض مرضى الدكتور فقط (للدكتور) - السطر 12
 router.get('/my-patients', doctorOnly, patientController.getDoctorPatients);
 
-// عرض مريض محدد
+// عرض مريض محدد - السطر 14
 router.get('/:id', patientBelongsToDoctor, patientController.getPatientById);
 
-// تحديث بيانات مريض (للاستقبال والدكتور)
+// تحديث بيانات مريض (للاستقبال والدكتور) - السطر 16
 router.put('/:id', patientBelongsToDoctor, patientController.updatePatient);
 
-// البحث عن مريض
+// البحث عن مريض - السطر 18
 router.get('/search/:query', patientController.searchPatients);
 
-// إحصائيات المرضى
+// إحصائيات المرضى - السطر 20
 router.get('/stats/summary', patientController.getPatientStats);
 
 module.exports = router;
